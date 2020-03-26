3d5920a3308047507415ef687ac34acd8acd6992
AndrewTHowell <howelldrew99@gmail.com>
2020-03-26 13:20:26 +0000

    UI fully modular and inputs verified

--- assignment.py
+++ assignment.py
@@ -2,6 +2,8 @@
 
 from RS import Recommender
 
+from math import inf as infinity
+
 # Section End
 
 # Section: Functions
@@ -46,6 +48,8 @@ class RecommendationUI:
                 while self.moodSet:
                     self.menu()
 
+        print("Goodbye")
+
     def login(self):
         print("\n\n*** Login page ***\n")
 
@@ -59,57 +63,54 @@ class RecommendationUI:
     def setMood(self):
         print("\n\n*** Mood Menu ***")
         print("\nWhat current mood are you in?\n")
-        print("Happy")
-        print("Sad")
-        print("Active")
-        print("Lazy\n")
-        self.mood = None
-        while self.mood not in ["happy", "sad", "active", "lazy"]:
-            self.mood = input("Enter mood: ").lower()
+
+        moods = ["happy", "sad", "active", "lazy"]
+        self.mood = self.getChoice("Enter mood", moods)["text"]
+
         print("\nMood set")
         print(f"You are feeling {self.mood}")
 
     def menu(self):
-        print("\n\n*** Main Menu ***")
-        print("1. Get recommendation")
-        print("2. Edit user profile")
-        print("7. Change mood")
-        print("8. Logout")
-        print("9. Exit\n")
+        print("\n\n*** Main Menu ***\n")
 
-        menuChoice = input("Enter choice: ")
+        choices = ["Get recommendation",
+                   "Edit user profile",
+                   "Change mood",
+                   "Logout",
+                   "Exit"]
+        while True:
+            choice = self.getChoice("Enter choice", choices)["num"]
 
-        if menuChoice == "1":
-            self.giveRecommendation()
+            if choice == 1:
+                self.giveRecommendation()
 
-        elif menuChoice == "2":
-            self.profileMenu()
+            elif choice == 2:
+                self.profileMenu()
 
-        elif menuChoice == "7":
-            self.moodSet = False
+            elif choice == 3:
+                self.moodSet = False
+                break
 
-        elif menuChoice == "8":
-            self.moodSet = False
-            self.loggedIn = False
+            elif choice == 4:
+                self.moodSet = False
+                self.loggedIn = False
+                break
 
-        elif menuChoice == "9":
-            self.moodSet = False
-            self.loggedIn = False
-            self.running = False
+            elif choice == 5:
+                self.moodSet = False
+                self.loggedIn = False
+                self.running = False
+                break
 
     def giveRecommendation(self):
-        recommendationSize = input("\nHow many recommended music tracks "
-                                   "would you like? (default is 5): ")
-        if recommendationSize.isdigit():
-            recommendationSize = int(recommendationSize)
-        else:
-            recommendationSize = 5
+        message = "How many recommended music tracks would you like?"
+        recommendationSize = self.getNum(message, 1, 30, 5)
 
         recommendedMusic = self.RS.getRecommendation(self.userID, self.mood,
                                                      recommendationSize)
 
         print(f"\nBased on your previous ratings, here are your "
-              "{recommendationSize} recommended music tracks:")
+              f"{recommendationSize} recommended music tracks:")
         counter = 1
         for musicTrack in recommendedMusic:
             title = musicTrack["title"]
@@ -121,54 +122,111 @@ class RecommendationUI:
         exitProfile = False
         while not exitProfile:
             print(f"\n\n*** User {self.userID} Menu ***")
-            print("\n** Ratings **")
+            print("\n** Ratings **\n")
 
             userRatings = self.RS.getUserRatings(self.userID, self.mood)
             for userRating in userRatings:
+                trackID = userRating["track"]["itemID"]
                 trackTitle = userRating["track"]["title"]
                 trackArtist = userRating["track"]["artist"]
                 rating = userRating["rating"]
                 print(f"You rated {trackTitle} by {trackArtist},"
-                      f" {rating} out of 5")
+                      f" {rating} out of 5 (Track ID: {trackID})")
+            if userRatings == []:
+                print("You haven't provided any ratings in this mood yet")
 
-            print("\n** Options **")
-            print("1. Add book rating")
-            print("2. Edit book rating")
-            print("3. Delete book rating")
-            print("9. Return to Main Menu")
+            print("\n** Options **\n")
 
-            menuChoice = input("\nEnter choice: ")
+            choices = ["Add book rating",
+                       "Edit book rating",
+                       "Delete book rating",
+                       "Return to Main Menu"]
 
-            if menuChoice == "1":
+            choice = self.getChoice("Enter choice", choices)["num"]
+
+            if choice == 1:
                 self.addRating()
 
-            elif menuChoice == "2":
+            elif choice == 2:
                 self.changeRating()
 
-            elif menuChoice == "3":
+            elif choice == 3:
                 self.deleteRating()
 
-            elif menuChoice == "9":
+            elif choice == 4:
                 exitProfile = True
 
     def addRating(self):
         itemID = int(input("Enter the music track ID: "))
-        rating = int(input("Enter rating (0-5): "))
+        rating = self.getNum("Enter rating", 0, 5)
         self.RS.addRating(self.userID, itemID, self.mood, rating)
 
     def changeRating(self):
         itemID = int(input("Enter the music track ID: "))
         currentRating = self.RS.getUserRating(self.userID, itemID, self.mood)
-        currentRating = currentRating["rating"]
-        print(f"Current rating is {currentRating}")
 
-        self.RS.deleteRating(self.userID, itemID, self.mood)
-        rating = int(input("Enter new rating (0-5): "))
-        self.RS.addRating(self.userID, itemID, self.mood, rating)
+        if currentRating is not None:
+            currentRating = currentRating["rating"]
+            print(f"Current rating is {currentRating}")
+
+            self.RS.deleteRating(self.userID, itemID, self.mood)
+            rating = self.getNum("Enter rating", 0, 5)
+            self.RS.addRating(self.userID, itemID, self.mood, rating)
+
+        else:
+            print("You have not rated this music track before")
+            input("Press any key to continue")
 
     def deleteRating(self):
         itemID = int(input("Enter the music track ID: "))
         self.RS.deleteRating(self.userID, itemID, self.mood)
+
+    def getNum(self, message, minimum=-infinity, maximum=infinity, default=False):
+        num = None
+        while num not in range(minimum, maximum):
+            minText = ""
+            if minimum != -infinity:
+                minText = f" (minimum: {minimum})"
+
+            maxText = ""
+            if maximum != infinity:
+                maxText = f" (maximum: {maximum})"
+
+            defaultText = ""
+            if default:
+                defaultText = f" (default: {default})"
+
+            num = input(f"{message}{minText}{maxText}{defaultText}: ")
+
+            if num == "":
+                return default
+
+            if num.isdigit():
+                num = int(num)
+
+        return int(num)
+
+    def getChoice(self, message, choices):
+        choices = [choice.lower() for choice in choices]
+
+        choiceRange = range(1, len(choices)+1)
+        for choiceIndex in choiceRange:
+            print(f"{choiceIndex}. {choices[choiceIndex-1].capitalize()}")
+        print()
+
+        while True:
+            choice = input(f"{message}: ")
+
+            if choice.isdigit():
+                choiceIndex = int(choice)
+                if choiceIndex in choiceRange:
+                    return {"num": choiceIndex,
+                            "text": choices[choiceIndex-1]}
+
+            if choice.lower() in choices:
+                return {"num": choices.index(choice)+1,
+                        "text": choice}
+
 
 # Section End
 
